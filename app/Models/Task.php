363b478d14496d3dbdac9626feb4e95ef89660c4
bhagat-abhishek <inst.abhishek@gmail.com>
2024-01-13 15:06:03 +0530

    Added relation

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
@@ -12,4 +13,12 @@ class Task extends Model
     protected $fillable = [
         'user_id', 'name'
     ];
+
+
+    // Relations
+
+    public function users(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
