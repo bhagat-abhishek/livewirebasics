3146f165b644f686cbafcc8a67288823fcb098cf
bhagat-abhishek <inst.abhishek@gmail.com>
2024-01-13 19:28:07 +0530

    Submit event change

--- resources/views/livewire/tasks/task-index.blade.php
+++ resources/views/livewire/tasks/task-index.blade.php
@@ -3,7 +3,8 @@
 
         <input type="text"
             class="rounded-md w-1/5 border-0 py-2 text-gray-900 placeholder:text-gray-400 sm:text-sm sm:leading-6"
-            wire:model.blur="name" />
+            wire:model.blur="name"
+            wire:keydown.enter="save" />
 
         <span class="absolute -ml-6 mt-2">
             <span x-text="$wire.name.length"></span>
